639e7ddccee2d092001506c3d2dbe80ca64ddd9c
Antonio De La Garza <antonio.delagarza96@gmail.com>
2020-02-10 21:08:44 -0600

    Added Tags and Performers Controllers

---
+++ BAM_Web_App_Solution/BAM_Web_App/Controllers/PerformersController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repo_Lib.Abstractions;
+using DB_Data.Models;
+
+
+namespace BAM_Web_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerformersController : ControllerBase
+    {
+        private readonly IPerformerRepo<Performers> _performerRepo;
+
+        public PerformersController(IPerformerRepo<Performers> performerRepo)
+        {
+            _performerRepo = performerRepo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Performers> Get()
+        {
+            return _performerRepo.GetPerformers();
+        }
+
+        [HttpGet("{GroupName}", Name = "Get")]
+        public IActionResult Get(string name)
+        {
+            var performer = _performerRepo.GetPerformers().FirstOrDefault(x => x.GroupName == name);
+            if (performer != null)
+                return Ok(performer);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody, Bind("GroupName, PerformanceType, HourlyRate, Rating, GroupPass")] Performers performer)
+        {
+            _performerRepo.AddPerformers(performer);
+
+            return CreatedAtRoute("Get", new { Name = performer.GroupName }, performer);
+        }
+
+        [HttpPut("{GroupName}")]
+        public IActionResult Put(string GroupName, [FromBody]Performers performer)
+        {
+            if (_performerRepo.GetPerformers().Any(x => x.GroupName == GroupName))
+            {
+                _performerRepo.ModifyPerformers(performer);
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }
+
+}
---
+++ BAM_Web_App_Solution/BAM_Web_App/Controllers/TagsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repo_Lib.Abstractions;
+using DB_Data.Models;
+
+
+namespace BAM_Web_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly ITagRepo<Tags> _tags;
+
+        public TagsController(ITagRepo<Tags> tags)
+        {
+            _tags = tags;
+        }
+
+        [HttpGet]
+        public IEnumerable<Tags> Get()
+        {
+            return _tags.GetTags();
+        }
+
+        [HttpGet("{TagId}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var tag = _tags.GetTags().FirstOrDefault(x => x.TagId == id);
+            if (tag != null)
+            {
+                return Ok(tag);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody, Bind("TagId, Tag, GroupName")] Tags tag)
+        {
+            _tags.AddTags(tag);
+
+            return CreatedAtAction("Get", new { Id = tag.TagId }, tag);
+        }
+
+        [HttpPut("{TagId}")]
+        public IActionResult Put(int tagId, [FromBody] Tags tag)
+        {
+            if (_tags.GetTags().Any(x => x.TagId == tagId))
+            {
+                _tags.ModifyTags(tag);
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+    }
+}
