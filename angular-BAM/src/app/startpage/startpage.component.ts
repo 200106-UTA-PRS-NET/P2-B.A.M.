3f14cf08f3d3e5a25aa810100221f611beba3ae5
BradyJacobson <u0993342@utah.edu>
2020-02-15 01:03:08 -0600

    Implemented all methods for venues into angular. Current sign in page can show each method off individually.

--- angular-BAM/src/app/startpage/startpage.component.ts
+++ angular-BAM/src/app/startpage/startpage.component.ts
@@ -14,9 +14,11 @@ import {StartpageService} from '../startpage.service';
 export class StartpageComponent implements OnInit {
   chosen : string;
 
-  username : string;
+  chosen2:string;
 
-  password : string;
+  testingUsername : string;
+
+  testingPassword : string;
   
   logins:login[] = null;
 
@@ -26,12 +28,25 @@ export class StartpageComponent implements OnInit {
     clientPass: ''
   };
 
-
+  testLogin: login = {
+    clientName: '',
+    location: '',
+    clientPass: ''
+  };
 
   choice(c:string): void {
     this.chosen = c;
+    this.chosen2 = '';
+    this.Login.location = '';
+    this.Login.clientPass = ''
   }
 
+  secondChoice(c:string): void {
+    this.chosen2 = c;
+    this.Login.clientName = '';
+    this.Login.location = '';
+    this.Login.clientPass = ''
+  }
   
 
   constructor(private startpageservice: StartpageService) { }
@@ -40,14 +55,44 @@ export class StartpageComponent implements OnInit {
     this.startpageservice.getClient()
     .then(response => this.logins=response);
   }
+
+  findSpecificVenues(): void{
+    this.testLogin.clientName = '';
+    this.testLogin.location = '';
+    this.testLogin.clientPass = '';
+    this.startpageservice.getSpecificClient(this.testingUsername)
+    .then(response => this.testLogin=response);
+    // this.startpageservice.getSpecificClient(this.testingUsername)
+    // .then(response => this.testLogin=response);
+    //this.testLogin.clientPass = "REDACTED: OBTAINED WITHOUT USING PASSWORD";
+  }
+
+  findPasswordVenues(): void{
+    this.testLogin.clientName = '';
+    this.testLogin.location = '';
+    this.testLogin.clientPass = '';
+    this.startpageservice.getPasswordClient(this.testingUsername, this.testingPassword)
+    .then(response => this.testLogin=response);
+    // this.startpageservice.getPasswordClient(this.testingUsername, this.testingPassword)
+    // .then(response => this.testLogin=response);
+  }
+
+
   register(): void{
     this.startpageservice.addClient(this.Login);
     this.chosen =  '';
+    this.chosen2 = '';
+  }
+
+  editVenue(): void{
+    this.startpageservice.editClient(this.Login,this.testingUsername);
+    this.chosen =  '';
+    this.chosen2 = '';
   }
 
   ngOnInit(): void {
-    this.username = "a";
-    this.password = "b";
+    this.testingUsername = '';
+    this.testingPassword = '';
   }
 
   private handleError(error: any): Promise<any> {
